dcf4d3bd042d2722bccb4f1da544b2a7365d73e3
Nyicolasz <125839438+Nyicolasz@users.noreply.github.com>
2023-03-01 16:30:53 -0300

    Add files via upload

---
+++ projetoweb_poo/automovies.class.php
@@ -0,0 +1,45 @@
+<?php
+
+class Automoveis{
+
+    //Criando os objetos privados
+    private $tipo;
+    private $cor;
+    private $placa;
+    private $nportas;
+
+    // Metodos de captura e de alteração dos valores dos objetos
+    public function getTipo(){
+        return $this-> tipo;
+    }
+
+    public function setTipo($tipo){
+        $this->tipo = $tipo;
+    }
+
+    public function getCor(){
+        return $this-> cor;
+    }
+
+    public function setCor($cor){
+        $this->cor = $cor;
+    }
+
+    public function getPlaca(){
+        return $this-> placa;
+    }
+
+    public function setPlaca($placa){
+        $this->placa = $placa;
+    }
+
+    public function getNportas(){
+        return $this-> nportas;
+    }
+
+    public function setNportas($nportas){
+        $this->nportas = $nportas;
+    }
+}
+
+?>
